ec7b40f5e5d9bd1b98d8e7e925d9151a202a412f
parisaderakhshan <parisa6derakhshan@gmail.com>
2023-08-06 13:23:31 +0330

    style input & button

--- components/Todo/Todo.jsx
+++ components/Todo/Todo.jsx
@@ -1,6 +1,7 @@
 import styles from "./Todo.module.css";
 import React from "react";
 import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 const Todo = () => {
   return (
@@ -9,13 +10,29 @@ const Todo = () => {
         <div className={styles.title}>
           <h1>Todo list</h1>
         </div>
-        <div className={styles.input}>
+        <div style={{ height: "55px" }} className={styles.input}>
           <TextField
-          style={{width:'100%'}}
+            style={{ width: "100%", height: "55px" }}
             id="outlined-basic"
             label="Enter your task"
             variant="outlined"
           />
+          <div
+            style={{
+              width: "35%",
+              alignItems: "center",
+              justifyContent: "center",
+              marginLeft: "10px",
+            }}
+            className={styles.add}
+          >
+            <Button
+              style={{ width: "100%", height: "55px", background: "#A9DDF8",color:"black" }}
+              variant="contained"
+            >
+              Add
+            </Button>
+          </div>
         </div>
       </div>
     </div>
